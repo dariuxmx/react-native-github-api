f0b7fd7c8ab8951b1782b302e81c8186f961d24a
dariuxmx <dario@habanerostudio.mx>
2019-12-10 22:47:36 -0500

    Flat list to commits in modal view

---
+++ components/CommitCell.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CellContainer, RepoName, CommitAuthor, DateUpdated, CommitMessage } from '../utils/Styles'
+
+class CommitCell extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isModalVisible: false
+        }
+    };
+
+    render(){
+        const { date, name, author, commitMessage } = this.props;
+        return(
+            <CellContainer>
+                <DateUpdated>{date}</DateUpdated>
+                <RepoName>{name}</RepoName>
+                <CommitAuthor>{author}</CommitAuthor>
+                <CommitMessage>{commitMessage}</CommitMessage>
+            </CellContainer>
+        );
+    }
+}
+
+export default CommitCell;
+
+CommitCell.propTypes = {
+    date: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    commitMessage: PropTypes.string.isRequired,
+  };
+  
